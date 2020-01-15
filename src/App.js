aad5a38071e5379c787596a0e3b018979fff2b86
Frank <frank@Franks-Mac.local>
2020-01-15 17:22:35 +0000

    add api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-
+// http://jsonplaceholder.typicode.com/users
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      mosters: [
-        {
-          name: "A"
-        },
-        {
-          name: "B"
-        },
-        {
-          name: "C"
-        }
-      ]
+      monsters: []
     };
+  }
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({monsters: users}))
+    
   }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map( monster => monster.map)}
+        {this.state.monsters.map(monster => 
+          <h1 key={monster.id}>{monster.name}</h1>
+        )}
       </div>
     );
   }
