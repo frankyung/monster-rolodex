d4c0546978f5a7b5482e3dac798ae32ecaf7f726
Frank Yung <frankyung1993@gmail.com>
2020-01-24 17:14:24 +0000

    Updates

---
+++ precache-manifest.c4d1bbb5b1726f7c0121d6a4a65579b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7833dd88bd2fd18c062d30158cf58b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c6b2332c1bfddc594569",
+    "url": "/monster-rolodex/static/css/main.d9516a09.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6b2332c1bfddc594569",
+    "url": "/monster-rolodex/static/js/main.08fbfa83.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
